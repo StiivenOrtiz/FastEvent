d875508726921686f7db769f09387031d9161fb8
StiivenOrtiz <stivenestortiz@gmail.com>
2023-05-01 21:33:37 -0500

    Creación de la plantilla para crear pantallas, correciones menores en palabras de otras pantallas.

---
+++ src/main/java/com/fastevent/fastevent/Controladores/TemplateController.java
@@ -0,0 +1,82 @@
+package com.fastevent.fastevent.Controladores;
+
+import com.fastevent.fastevent.Controladores.Controlador;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class TemplateController extends Controlador {
+
+    /*
+        Variables
+     */
+
+    // Métodos de la barra de navegación
+
+    public void botonLogoRectangularPresionado(ActionEvent actionEvent) {
+        System.out.println("Logo rectangular presionado");
+    }
+
+    public void accionBotonInicio(ActionEvent actionEvent) {
+        System.out.println("Inicio");
+    }
+
+    public void accionBotonPrecios(ActionEvent actionEvent) {
+        System.out.println("Precios");
+    }
+
+    public void accionBotonNosotros(ActionEvent actionEvent) {
+        System.out.println("Nosotros");
+    }
+
+    public void accionBotonContactenos(ActionEvent actionEvent) {
+        System.out.println("Contactenos");
+    }
+
+    public void accionBotonBoleteria(ActionEvent actionEvent) {
+        System.out.println("Boleteria");
+    }
+
+    public void botonIniciarSesion(ActionEvent actionEvent) {
+        System.out.println("Iniciar sesion");
+    }
+
+    public void botonRegistrarse(ActionEvent actionEvent) {
+        System.out.println("Registrarse");
+    }
+
+    // Métodos del footer
+
+    public void accionBotonTwitter(ActionEvent actionEvent) {
+        System.out.println("Twitter");
+    }
+
+    public void accionBotonLinkedin(ActionEvent actionEvent) {
+        System.out.println("Linkedin");
+    }
+
+    public void botonTerminos(ActionEvent actionEvent) {
+        System.out.println("Terminos");
+    }
+
+    public void botonPrivacidad(ActionEvent actionEvent) {
+        System.out.println("Privacidad");
+    }
+
+    public void accionBotonFacebook(ActionEvent actionEvent) {
+        System.out.println("Facebook");
+    }
+
+    public void accionBotonInstagram(ActionEvent actionEvent) {
+        System.out.println("Instagram");
+    }
+
+    public void accionBotonPreguntasFrecuentes(ActionEvent actionEvent) {
+        System.out.println("Preguntas frecuentes");
+    }
+
+    /*
+        Metodos de la interfaz
+     */
+}
