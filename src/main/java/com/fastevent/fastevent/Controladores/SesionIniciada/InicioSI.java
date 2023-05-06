7c629d146f5c834bfd20cf48fbed2a5e62375143
StiivenOrtiz <stivenestortiz@gmail.com>
2023-05-06 09:15:56 -0500

    InicioSI v0.2

--- src/main/java/com/fastevent/fastevent/Controladores/SesionIniciada/InicioSI.java
+++ src/main/java/com/fastevent/fastevent/Controladores/SesionIniciada/InicioSI.java
@@ -8,9 +8,12 @@ import javafx.fxml.FXML;
 import javafx.geometry.Side;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
-import javafx.scene.control.CustomMenuItem;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.stage.Popup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 
 public class InicioSI extends Controlador {
 
@@ -97,9 +100,9 @@ public class InicioSI extends Controlador {
     public void accionBotonMisEventos(ActionEvent actionEvent) {
         System.out.println("Mis eventos");
 
-        notifStatus.set(true);
         String nuevaNotif = "Se ha recibido una nueva notificación";
-        notifList.getItems().add(nuevaNotif);
+        listaDeNotificaciones.getItems().add(new xd(nuevaNotif, "https://www.google.com/"));
+        estadoDeNotifaciones.set(true);
     }
 
     /*
@@ -107,37 +110,134 @@ public class InicioSI extends Controlador {
      */
 
     @FXML
-    private Button notifButton;
+    private Button botonDeNotificacion;
     @FXML
-    private ListView<String> notifList;
+    private ListView<xd> listaDeNotificaciones;
     @FXML
-    ContextMenu notifMenu;
+    ContextMenu notificaciones = new ContextMenu();
+    @FXML
+    public Button botonBorrarNotificaciones;
 
-    private BooleanProperty notifStatus = new SimpleBooleanProperty(false);
+    private final BooleanProperty estadoDeNotifaciones = new SimpleBooleanProperty(false);
 
     public void initialize() {
         // Agregar el ChangeListener al estado de la notificación
-        notifStatus.addListener((obs, oldVal, newVal) -> {
-            if (newVal) {
-                notifButton.getStyleClass().add("notification");
-            } else {
-                notifButton.getStyleClass().remove("notification");
-            }
+        estadoDeNotifaciones.addListener((obs, oldVal, newVal) -> {
+            if (newVal)
+                botonDeNotificacion.getStyleClass().add("notification");
+            else
+                botonDeNotificacion.getStyleClass().remove("notification");
         });
 
-        // Crear el ContextMenu
-        notifMenu.setAutoHide(true);
-        notifMenu.getItems().add(new CustomMenuItem(notifList));
-
-        // Configurar el menú contextual para mostrar el ListView cuando se abre
-
+        // Configurar el estilo de los elementos del ListView
+        estiloElementosListView();
 
         // Configurar el botón de notificación para mostrar el menú contextual
-        notifButton.setContextMenu(notifMenu);
+        botonDeNotificacion.setContextMenu(notificaciones);
+
+        // Agregar imagen al boton de borrar notificaciones
+        agregarImagenBotonBorrarNotificaciones();
+
     }
 
-    public void showNotifContextMenu(ActionEvent actionEvent) {
+    public void mostrarNotificaciones(ActionEvent actionEvent) {
         // Mostrar el menú contextual cuando se hace clic en el botón de notificación
-        notifButton.getContextMenu().show(notifButton, Side.BOTTOM, 0, 0);
+        botonDeNotificacion.getContextMenu().show(botonDeNotificacion, Side.BOTTOM, 0, 0);
+        estadoDeNotifaciones.set(false);
+    }
+
+    public void borrarNotificaciones(ActionEvent actionEvent) {
+        listaDeNotificaciones.getItems().clear();
+    }
+
+    public void imprimirNumeroDeElementoSeleccionado(MouseEvent mouseEvent) {
+        // Obtener el índice del elemento seleccionado
+        int indice = listaDeNotificaciones.getSelectionModel().getSelectedIndex();
+        // Imprimir el número del índice del elemento seleccionado
+        System.out.println("Elemento seleccionado en el índice: " + listaDeNotificaciones.getItems().get(indice).getDireccion());
+    }
+
+    protected void agregarImagenBotonBorrarNotificaciones() {
+        // Cargar la imagen original
+        Image originalImage = new Image("com/fastevent/fastevent/SesionIniciada/Inicio/nav-bar/icono_borrar-notificaciones.png");
+        ImageView originalImageView = new ImageView(originalImage);
+
+        // Cargar la imagen de hover
+        Image hoverImage = new Image("com/fastevent/fastevent/SesionIniciada/Inicio/nav-bar/icono_borrar-notificaciones_seleccionado.png");
+        ImageView hoverImageView = new ImageView(hoverImage);
+
+        originalImageView.setFitHeight(26);
+        originalImageView.setFitWidth(26);
+
+        // Configurar el botón con la imagen original
+        botonBorrarNotificaciones.setGraphic(originalImageView);
+
+        hoverImageView.setFitHeight(26);
+        hoverImageView.setFitWidth(26);
+
+        // Cambiar la imagen al pasar el mouse por encima
+        botonBorrarNotificaciones.setOnMouseEntered(event -> {
+            botonBorrarNotificaciones.setGraphic(hoverImageView);
+        });
+
+        // Volver a la imagen original al sacar el mouse
+        botonBorrarNotificaciones.setOnMouseExited(event -> {
+            botonBorrarNotificaciones.setGraphic(originalImageView);
+        });
+    }
+
+    protected void estiloElementosListView() {
+        listaDeNotificaciones.setCellFactory(list -> {
+            ListCell<xd> cell = new ListCell<xd>() {
+                @Override
+                protected void updateItem(xd item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty || item == null) {
+                        setText(null);
+                    } else {
+                        setText(item.getNombre());
+                    }
+                }
+            };
+            cell.setStyle("-fx-control-inner-background: black; -fx-text-fill: white; -fx-background-color: black;");
+            cell.setOnMouseEntered(e -> cell.setStyle("-fx-control-inner-background: #CFB14F; -fx-text-fill: black; -fx-background-color: #CFB14F;"));
+            cell.setOnMouseExited(e -> cell.setStyle("-fx-control-inner-background: black; -fx-text-fill: white; -fx-background-color: black;"));
+            cell.setOnMouseClicked(e -> cell.setStyle("-fx-control-inner-background: black; -fx-text-fill: white; -fx-background-color: black;"));
+            cell.selectedProperty().addListener((obs, oldVal, newVal) -> {
+                if (newVal) {
+                    cell.setStyle("-fx-control-inner-background: black; -fx-text-fill: white; -fx-background-color: black;");
+                } else {
+                    cell.setStyle("-fx-control-inner-background: black; -fx-text-fill: white; -fx-background-color: black;");
+                }
+            });
+            return cell;
+        });
+    }
+
+    public static class xd {
+        private String nombre;
+        private String direccion;
+
+        public xd(String nombre, String direccion) {
+            this.nombre = nombre;
+            this.direccion = direccion;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public String getDireccion() {
+            return direccion;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public void setDireccion(String direccion) {
+            this.direccion = direccion;
+        }
     }
 }
+
